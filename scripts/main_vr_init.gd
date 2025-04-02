bf73fbbe0c32d3b05135e54977935c09d201f242
uno1982 <jmanc82@aim.com>
2025-04-02 17:47:40 -0500

    Update shadow quality Properly detect framerate and apply highest (120fps)

--- scripts/main_vr_init.gd
+++ scripts/main_vr_init.gd
@@ -1,6 +1,15 @@
 extends Node3D
 var xr_interface: XRInterface
 
+## XR frame rate
+var xr_frame_rate : float = 0
+
+## Physics rate multiplier compared to HMD frame rate
+@export var physics_rate_multiplier : int = 1
+
+## If non-zero, specifies the target refresh rate
+@export var target_refresh_rate : float = 120
+
 #this is the main init script for a VR Scene
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
@@ -8,7 +17,53 @@ func _ready():
 	if xr_interface and xr_interface.is_initialized():
 		print("OpenXR init successfully")
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
-		
+		# Set the XR frame rate
+		_set_xr_frame_rate()
 		get_viewport().use_xr = true
 	else:
 		print("HMD Not Connected! Check Meta link or Steamvr connection")
+		
+# Set the XR frame rate to the configured value
+func _set_xr_frame_rate() -> void:
+	# Get the reported refresh rate
+	xr_frame_rate = xr_interface.get_display_refresh_rate()
+	if xr_frame_rate > 0:
+		print("StartXR: Refresh rate reported as ", str(xr_frame_rate))
+	else:
+		print("StartXR: No refresh rate given by XR runtime")
+
+	# Pick a desired refresh rate
+	var desired_rate := target_refresh_rate if target_refresh_rate > 0 else xr_frame_rate
+	var available_rates : Array = xr_interface.get_available_display_refresh_rates()
+	if available_rates.size() == 0:
+		print("StartXR: Target does not support refresh rate extension")
+	elif available_rates.size() == 1:
+		print("StartXR: Target supports only one refresh rate")
+	elif desired_rate > 0:
+		print("StartXR: Available refresh rates are ", str(available_rates))
+		var rate = _find_closest(available_rates, desired_rate)
+		if rate > 0:
+			print("StartXR: Setting refresh rate to ", str(rate))
+			xr_interface.set_display_refresh_rate(rate)
+			xr_frame_rate = rate
+
+	# Pick a physics rate
+	var active_rate := xr_frame_rate if xr_frame_rate > 0 else 144.0
+	var physics_rate := int(round(active_rate * physics_rate_multiplier))
+	print("StartXR: Setting physics rate to ", physics_rate)
+	Engine.physics_ticks_per_second = physics_rate
+	
+# Find the closest value in the array to the target
+func _find_closest(values : Array, target : float) -> float:
+	# Return 0 if no values
+	if values.size() == 0:
+		return 0.0
+
+	# Find the closest value to the target
+	var best : float = values.front()
+	for v in values:
+		if abs(target - v) < abs(target - best):
+			best = v
+
+	# Return the best value
+	return best
